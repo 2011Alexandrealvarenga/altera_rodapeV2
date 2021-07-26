f437f0aa7c3082109301cfe4cc330f405896ff06
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-26 17:14:50 -0300

    add rodape

---
+++ altera_rodapev2.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Plugin Name: Alterar Rodape v2
+Plugin URI: www.alexandre.com
+Description: Alterar Rodape
+Version: 1.0
+Author: alexandre
+Author URI: www.alexandre.com
+License: GLPV2
+Text Domain: primeiro plugin
+*/
+
+function meu_plugin_altera_rodape(){
+    echo 'meu primeiro plugin Alexandre';
+}
+
+add_action('wp_footer','meu_plugin_altera_rodape');
